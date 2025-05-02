a227ef64e56caa88699ef72d0efc8ab6f19bc156
varivashyap <119163421+varivashyap@users.noreply.github.com>
2025-05-02 19:32:57 +0530

    Update

--- services/HomeTimelineService/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Set environment variables for OpenWhisk
-export SERVICE_PORT=9091
-
-# Run the HotelTimelineService application
-exec /app/build/HotelTimelineService
--- services/PostStorageService/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Set environment variables for OpenWhisk
-export SERVICE_PORT=9091
-
-# Run the PostStorageService application
-exec /app/build/PostStorageService
-a
